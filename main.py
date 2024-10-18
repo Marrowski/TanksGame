cbe3d3657691941287feed28be7e6f330cc625e2
Oleh Yemelianov <emelianov2811@gmail.com>
2024-10-19 01:16:49 +0300

    Started to work

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+import os
+import random
+from modules.classes import *
+from modules.mapsetting import map
+
+pygame.init()
+
+
+
+is_game_running = True
+
+while is_game_running:
+    window.fill((255,0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_game_running = False
+
+    pygame.display.flip()
+
+
